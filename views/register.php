6841aa0f1a58d617a3818c5f8e9d4a0735238742
aimad bachar <aimadbacharpro@outlook.fr>
2021-11-22 09:44:06 +0100

    create register form

---
+++ views/register.php
@@ -0,0 +1,24 @@
+<h3>Contact us</h3>
+<form action="" method="post">
+    <div class="mb-3">
+        <label for="firstname" class="form-label">Prénom</label>
+        <input type="text" class="form-control" name="firstname" id="firstname">
+    </div>
+    <div class="mb-3">
+        <label for="name" class="form-label">Nom</label>
+        <input type="text" class="form-control" name="name" id="name">
+    </div>
+    <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" name="email" id="email">
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Mot de passe</label>
+        <input type="password" class="form-control" name="password" id="password">
+    </div>
+    <div class="mb-3">
+        <label for="password2" class="form-label">Confirmer mot de passe</label>
+        <input type="password" class="form-control" name="password2" id="password2">
+    </div>
+    <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+</form>
