1c74bb7657fee33b7f0a462bb842c35b2ef57133
aimad bachar <aimadbacharpro@outlook.fr>
2021-11-22 09:43:54 +0100

    create login form

---
+++ views/login.php
@@ -0,0 +1,12 @@
+<h3>Contact us</h3>
+<form action="" method="post">
+    <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" name="email" id="email">
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Mot de passe</label>
+        <input type="password" class="form-control" name="password" id="password">
+    </div>
+    <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+</form>
