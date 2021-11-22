45a2338cca5cb5393d6cbed63d3022242429b6de
aimad bachar <aimadbacharpro@outlook.fr>
2021-11-22 16:04:39 +0100

    create class LoginModel

---
+++ models/loginModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\models;
+
+use App\Core\Model;
+
+class LoginModel extends Model
+{   
+    public string $email = '';
+    public string $password = '';
+
+    public function connect()
+    {
+        // création d'un user en base de donnée
+        echo "You are connected";
+    }
+
+    public function rules(): array
+    {
+        return [
+            'email' =>  [self::RULE_REQUIRED, self::RULE_EMAIL],
+            'password' =>  [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8], [self::RULE_MAX, 'max' => 20]]
+        ];
+    }
+}
