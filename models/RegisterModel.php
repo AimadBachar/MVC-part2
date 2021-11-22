e8fbc571613081e01a4f9d0a46e911b3fb8fd4b9
aimad bachar <aimadbacharpro@outlook.fr>
2021-11-22 11:21:23 +0100

    create register model class

---
+++ models/RegisterModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\models;
+
+class RegisterModel
+{   
+    public string $firstname;
+    public string $lastname;
+    public string $email;
+    public string $password;
+    public string $password_confirm;
+
+
+    
+}
