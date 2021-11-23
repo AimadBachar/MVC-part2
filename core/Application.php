32fba7212dfcf650dfabeaeaa85e9cd6a8766f32
aimad bachar <aimadbacharpro@outlook.fr>
2021-11-23 11:45:29 +0100

    instance new database with config

--- core/Application.php
+++ core/Application.php
@@ -4,6 +4,8 @@ namespace App\Core;
 
 use App\Core\Router;
 use App\Core\Response;
+use App\Core\Request;
+use App\Core\Database;
 
 class Application
 {
@@ -12,19 +14,22 @@ class Application
     public Router $router;
     public Request $request;
     public Response $response;
+    public Database $db;
 
     /**
      * Application constructor.
      * @param string $rootDir
      * @return void
      */
-    public function __construct(string $rootPath)
+    public function __construct(string $rootPath, array $config)
     {   
         self::$app = $this;
         self::$ROOT_DIR = $rootPath;
         $this->request = new Request();
         $this->response = new Response();
         $this->router = new Router($this->request, $this->response);
+
+        $this->db = new Database($config['db']);
     }
 
     /**
